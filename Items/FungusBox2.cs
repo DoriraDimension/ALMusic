5f9fc84ce3c34e741962879796e914bbb526e02d
Dorirā <thehackedphantom@gmail.com>
2025-06-23 16:16:54 +0200

    Same but for music mod

---
+++ Items/FungusBox2.cs
@@ -0,0 +1,46 @@
+﻿using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using ALMusic.Tiles;
+using System;
+
+namespace ALMusic.Items
+{
+    public class FungusBox2 : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot(Mod, "Sounds/Music/Fungus2"), ModContent.ItemType<FungusBox2>(), ModContent.TileType<FungusBox2Placed>());
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.autoReuse = true;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<FungusBox2Placed>();
+            Item.width = 32;
+            Item.height = 30;
+            Item.rare = ItemRarityID.LightRed;
+            Item.accessory = true;
+        }
+
+        public override void AddRecipes()
+        {
+            Mod AL = ALMusic.Instance.AL;
+            if (AL != null)
+            {
+                CreateRecipe()
+                .AddIngredient(ItemID.MusicBox)
+                .AddIngredient(AL.Find<ModItem>("GlowingMushmatter").Type, 4)
+                .AddIngredient(ItemID.GlowingMushroom, 60)
+                .AddTile(TileID.HeavyWorkBench)
+                .Register();
+            }
+        }
+    }
+}
